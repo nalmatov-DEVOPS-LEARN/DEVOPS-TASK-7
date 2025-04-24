c6a6a2c38d4fefd892e729597a74250f2c4c0926
nalmatov <nalmatov6@gmail.com>
2025-04-24 19:13:17 +0600

    test for cd

--- nodejs/ci/build_run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-docker build -t nurs_task_3 .
-
-# for daemon mode add "-d" flag
-docker run -p 8080:8080 -d --name nurs_task_3_container nurs_task_3
--- nodejs/ci/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-docker stop nurs_task_3_container
-
-docker rm nurs_task_3_container
-
-docker rmi nurs_task_3
